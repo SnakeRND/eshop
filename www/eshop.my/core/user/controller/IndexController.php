9c68072e04e12082d619c6d4f069f56b43e55d9d
a.androsov <alexandr.v.androsov@gmail.com>
2021-03-17 20:46:17 +0300

    upd

---
+++ www/eshop.my/core/user/controller/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace core\user\controller;
+
+use core\base\controller\BaseController;
+
+class IndexController extends BaseController {
+
+    protected function inputData(){
+        $template = $this->render(false, ['name' => 'Jon Snow']);
+    }
+
+}
---
+++ www/eshop.my/index.php
@@ -0,0 +1,27 @@
+Hello from Docker
+
+
+<?php
+define('VG_ACCESS', true);
+
+header('Content-Type:text/html:charset=utf-8');
+session_start();
+
+require_once 'config.php';
+require_once 'core/base/settings/internal_settings.php';
+require_once 'libraries/functions.php';
+
+use core\base\exceptions\RouteException;
+use core\base\controller\RouteController;
+
+
+try {
+
+    RouteController::getInstance()->route();
+
+}
+catch (RouteException $e){
+
+    exit($e->getMessage());
+
+}
